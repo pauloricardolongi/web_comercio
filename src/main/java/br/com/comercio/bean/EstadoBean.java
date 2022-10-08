19bf00ea3188acd0ff0185491a7944fd004fcbba
pauloricardolongi <pauloricardolongi@gmail.com>
2022-10-08 01:55:01 -0300

    teste classe bean com estado.xhtml

---
+++ src/main/java/br/com/comercio/bean/EstadoBean.java
@@ -0,0 +1,14 @@
+package br.com.comercio.bean;
+
+import javax.faces.bean.ManagedBean;
+import org.omnifaces.util.Messages;
+
+@ManagedBean
+public class EstadoBean {
+	public void salvar() {
+		
+		Messages.addGlobalInfo("Programação Web java");
+	}
+	
+
+}
